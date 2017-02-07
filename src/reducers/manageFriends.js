c4255b6a1e8a16e0bbe265a35124f4d983143161
cy2003 <catyyee@hotmail.com>
2017-02-06 20:59:55 -0500

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  if (action.type === 'ADD_FRIEND'){
+    return Object.assign({}, state, {friends: state.friends.concat(action.payload)})
+    // or return {...state, friends:[...state.friends, action.payload]}
+    // the spread operator - future JS.
+    // spread operator doesn't change it's original state
+  }
+  else if (action.type === 'REMOVE_FRIEND'){
+    var selectedFriend = state.friends.findIndex(friend => friend.id === action.payload)
+    var newFriendsArray = state.friends.slice(0, selectedFriend).concat(state.friends.slice(selectedFriend+1))
+    return Object.assign({}, state, {friends: newFriendsArray})
+  }
+  else {
+    return state
+  }
 }
